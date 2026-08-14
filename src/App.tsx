0ff2d3a8853aefd969d76b4521fdd6a8142fbcb3
Mykhailo Ivankiv <m.ivankiv@mil.ua>
2026-08-14 16:49:48 +0300

    Refactor CoordinateInput component: improve formatting and clean up code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const CoordinateInput = () => {
-    const [text, setText] = useState('');
-    const result = {
-        text,
-        result: {
-            latitude: 0,
-            longitude: 0,
-        },
-        errors: null
-    }
-    return <div>
-        <input
-        type="text" className="p-2 w-full border rounded-sm"
+  const [text, setText] = useState("");
+  const result = {
+    text,
+    result: { latitude: 0, longitude: 0 },
+    errors: null,
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        className="p-2 w-full border rounded-sm"
         placeholder="Enter coordinates"
         value={text}
         onChange={(e) => setText(e.target.value)}
-    />
-        <div className="whitespace-pre-wrap font-mono">
-            {JSON.stringify(result, null, 2)}
-        </div>
-    </div>;
-}
-
+      />
+      <div className="whitespace-pre-wrap font-mono">{JSON.stringify(result, null, 2)}</div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <main className="max-w-[60ch] p-4 m-auto">
       <h2>Тестовий опис</h2>
-      <CoordinateInput/>
+      <CoordinateInput />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
